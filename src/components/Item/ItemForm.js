211e2c2d1ada612e60c60a19ff007b81b3c545fa
arvind <bonandrion@gmail.com>
2018-06-11 11:00:53 -0600

    addeed app icon, created forms for items, po line and vendor, and added en translation

--- src/components/Item/ItemForm.js
+++ src/components/Item/ItemForm.js
@@ -7,59 +7,32 @@ import TextArea from '@folio/stripes-components/lib/TextArea';
 import { Required } from '../../Utils/Validate';
 
 class FundForm extends Component {
-  constructor(props) {
-    super(props);
-    this.renderForm = this.renderForm.bind(this);
-    this.renderSubForm = this.renderSubForm.bind(this);
-  }
-
-  renderForm = ({ fields }) => {
-    return (
-      <Row>
-        <Col xs={12}>
-          {fields.length === 0 &&
-            <Col xs={6}>
-              <div><em>- Please add agreements -</em></div>
-            </Col>
-          }
-          {fields.map(this.renderSubForm)}
-        </Col>
-        <Col xs={12} style={{ paddingTop: '10px' }}>
-          <Button onClick={() => fields.push({})}>+ Add</Button>
-        </Col>
-      </Row>
-    );
-  }
-
-  renderSubForm = (elem, index, fields) => {
-    return (
-      <Row key={index}>
-        <Col xs={12} md={4}>
-          <Field label="Name" name={`${elem}.name`} id={`${elem}.name`} validate={[Required]} component={TextField} fullWidth />
-        </Col>
-        <Col xs={12} md={4}>
-          <Field label="Discount %" name={`${elem}.discount`} id={`${elem}.discount`} type="number" component={TextField} fullWidth />
-        </Col>
-        <Col xs={12} md={4}>
-          <Field label="URL" name={`${elem}.reference_url`} id={`${elem}.reference_url`} type="text" component={TextField} fullWidth />
-        </Col>
-        <Col xs={12} md={10}>
-          <Field label="Notes" name={`${elem}.notes`} id={`${elem}.notes`} component={TextArea} fullWidth />
-        </Col>
-        <Col xs={12} md={2} style={{ textAlign: 'right' }}>
-          <Button onClick={() => fields.remove(index)} buttonStyle="danger">
-            Remove
-          </Button>
-        </Col>
-      </Row>
-    );
-  }
-
   render() {
     return (
       <Row>
+        <Col xs={6}>
+          <Field label="Title" name="title" id="title" component={TextField} fullWidth />
+        </Col>
+        <Col xs={6}>
+          <Field label="Receiving Note" name="receiving_note" id="receiving_note" component={TextField} fullWidth />
+        </Col>
+        <Col xs={6}>
+          <Field label="Product ID" name="product_id" id="product_id" component={TextArea} style={{ height: '82px' }} fullWidth />
+        </Col>
+        <Col xs={6}>
+          <Field label="Subscription From" name="subscription_from" id="subscription_from" component={TextField} fullWidth />
+        </Col>
+        <Col xs={6}>
+          <Field label="Material Type" name="subscription_from" id="subscription_from" component={TextField} fullWidth />
+        </Col>
+        <Col xs={6}>
+          <Field label="Subscription Interval" name="subscription_interval" id="subscription_interval" component={TextField} fullWidth />
+        </Col>
+        <Col xs={6}>
+          <Field label="Subscription To" name="subscription_to" id="subscription_to" component={TextField} fullWidth />
+        </Col>
         <Col xs={12}>
-          <FieldArray label="Agreements" name="agreements" id="agreements" component={this.renderForm} />
+          <Field label="Description" name="description" id="description" component={TextArea} fullWidth />
         </Col>
       </Row>
     );
--- src/components/Vendor/VendorForm.js
+++ src/components/Vendor/VendorForm.js
@@ -7,59 +7,21 @@ import TextArea from '@folio/stripes-components/lib/TextArea';
 import { Required } from '../../Utils/Validate';
 
 class VendorForm extends Component {
-  constructor(props) {
-    super(props);
-    this.renderForm = this.renderForm.bind(this);
-    this.renderSubForm = this.renderSubForm.bind(this);
-  }
-
-  renderForm = ({ fields }) => {
-    return (
-      <Row>
-        <Col xs={12}>
-          {fields.length === 0 &&
-            <Col xs={6}>
-              <div><em>- Please add agreements -</em></div>
-            </Col>
-          }
-          {fields.map(this.renderSubForm)}
-        </Col>
-        <Col xs={12} style={{ paddingTop: '10px' }}>
-          <Button onClick={() => fields.push({})}>+ Add</Button>
-        </Col>
-      </Row>
-    );
-  }
-
-  renderSubForm = (elem, index, fields) => {
-    return (
-      <Row key={index}>
-        <Col xs={12} md={4}>
-          <Field label="Name" name={`${elem}.name`} id={`${elem}.name`} validate={[Required]} component={TextField} fullWidth />
-        </Col>
-        <Col xs={12} md={4}>
-          <Field label="Discount %" name={`${elem}.discount`} id={`${elem}.discount`} type="number" component={TextField} fullWidth />
-        </Col>
-        <Col xs={12} md={4}>
-          <Field label="URL" name={`${elem}.reference_url`} id={`${elem}.reference_url`} type="text" component={TextField} fullWidth />
-        </Col>
-        <Col xs={12} md={10}>
-          <Field label="Notes" name={`${elem}.notes`} id={`${elem}.notes`} component={TextArea} fullWidth />
-        </Col>
-        <Col xs={12} md={2} style={{ textAlign: 'right' }}>
-          <Button onClick={() => fields.remove(index)} buttonStyle="danger">
-            Remove
-          </Button>
-        </Col>
-      </Row>
-    );
-  }
-
   render() {
     return (
       <Row>
-        <Col xs={12}>
-          <FieldArray label="Agreements" name="agreements" id="agreements" component={this.renderForm} />
+        <Col xs={6}>
+          <Field label="Vendor Ref Number" name="ref_number" id="ref_number" component={TextField} fullWidth />
+        </Col>
+        <Col xs={6}>
+          <Field label="Vendor Ref Type" name="ref_no_type" id="ref_no_type" component={TextField} fullWidth />
+        </Col>
+        <Col xs={6}>
+          <Field label="Instructions to Vendor" name="instructions" id="instructions" component={TextArea} style={{ height: '82px' }} fullWidth />
+        </Col>
+        <Col xs={6}>
+          <Field label="Vendor Account" name="vendor_account" id="vendor_account" component={TextField} fullWidth />
+          <Field label="Note from vendor" name="note_from_vendor" id="note_from_vendor" component={TextField} fullWidth />
         </Col>
       </Row>
     );
