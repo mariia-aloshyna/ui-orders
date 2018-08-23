a29df5509b2508dd62dff8e484bf45937f88a55a
arvind <bonandrion@gmail.com>
2018-08-23 14:43:40 -0600

    disabled fund distribution components

--- src/components/PurchaseOrder/PO/PO.js
+++ src/components/PurchaseOrder/PO/PO.js
@@ -155,7 +155,7 @@ class PO extends Component {
 
     return (
       <Pane id="pane-podetails" defaultWidth="fill" paneTitle={_.get(initialValues, ['name'], '')} lastMenu={lastMenu} dismissible onClose={this.props.onClose}>
-        <FundDistribution openReceiveItem={this.openReceiveItem} openReceived={this.openReceived} />
+        {/* <FundDistribution openReceiveItem={this.openReceiveItem} openReceived={this.openReceived} /> */}
         <Row end="xs"><Col xs><ExpandAllButton accordionStatus={this.state.sections} onToggle={this.handleExpandAll} /></Col></Row>
         <AccordionSet accordionStatus={this.state.sections} onToggle={this.onToggleSection}>
           <Accordion label="Purchase Order" id="purchaseOrder">
