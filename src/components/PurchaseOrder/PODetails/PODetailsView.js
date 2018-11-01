d0ae91f5e538f8ae9c620231292428e202590e6f
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2018-11-01 17:10:37 +0300

    UIOR-23 align Order details view to mockup
    Closes UIOR-23
    Closes UIOR-32

--- src/components/PurchaseOrder/PODetails/PODetailsView.js
+++ src/components/PurchaseOrder/PODetails/PODetailsView.js
@@ -7,28 +7,36 @@ import FormatDate from '../../Utils/FormatDate';
 
 class DetailsView extends React.Component {
   static propTypes = {
-    initialValues: PropTypes.object
-  }
+    order: PropTypes.object.isRequired,
+  };
 
   render() {
-    const { initialValues } = this.props;
-    const dataVal = initialValues || false;
+    const { order } = this.props;
     return (
       <Row>
-        <Col xs={3}>
-          <KeyValue label="Vendor" value={_.get(dataVal, 'vendor')} />
+        <Col xs={6}>
+          <KeyValue label="Vendor" value={_.get(order, 'vendor_name')} />
         </Col>
-        <Col xs={3}>
-          <KeyValue label="PO Number" value={_.get(dataVal, 'po_number')} />
+        <Col xs={6}>
+          <KeyValue label="Created By" value={_.toString(_.get(order, 'created_by'))} />
         </Col>
-        <Col xs={3}>
-          <KeyValue label="Assigned To Name" value={_.get(dataVal, 'assigned_to')} />
+        <Col xs={6}>
+          <KeyValue label="PO Number" value={_.get(order, 'po_number')} />
         </Col>
-        <Col xs={3}>
-          <KeyValue label="Created On" value={FormatDate(_.toString(_.get(dataVal, 'created')))} />
+        <Col xs={6}>
+          <KeyValue label="Created On" value={FormatDate(_.toString(_.get(order, 'created')))} />
         </Col>
-        <Col xs={3}>
-          <KeyValue label="Created By" value={_.toString(_.get(dataVal, 'created_by'))} />
+        <Col xs={6}>
+          <KeyValue label="Assigned To" value={_.get(order, 'assigned_to_user')} />
+        </Col>
+        <Col xs={6}>
+          <KeyValue label="Bill To" value={null} />
+        </Col>
+        <Col xs={6}>
+          <KeyValue label="Ship To" value={null} />
+        </Col>
+        <Col xs={6}>
+          <KeyValue label="Order Type" value={_.get(order, 'order_type')} />
         </Col>
       </Row>
     );
