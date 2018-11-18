849e518d3b1b1668dc245b089be28f7f45d894e7
Jeffrey Cherewaty <jeffrey@frontside.io>
2018-11-18 13:32:34 -0600

    Replace stripes.intl.formatMessage with FormattedMessage

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,29 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
 import { Settings } from '@folio/stripes/smart-components';
 import GeneralSettings from './general-settings';
 import SomeFeatureSettings from './some-feature-settings';
 
-/*
-  STRIPES-NEW-APP
-  Your app's settings pages are defined here.
-  The pages "general" and "some feature" are examples. Name them however you like.
-*/
-
 export default class OrdersSettings extends React.Component {
-  static propTypes = {
-    stripes: PropTypes.object,
-  }
-
   pages = [
     {
       route: 'general',
-      label: this.props.stripes.intl.formatMessage({ id: 'ui-orders.settings.general' }),
+      label: <FormattedMessage id="ui-orders.settings.general" />,
       component: GeneralSettings,
     },
     {
       route: 'somefeature',
-      label: this.props.stripes.intl.formatMessage({ id: 'ui-orders.settings.some-feature' }),
+      label: <FormattedMessage id="ui-orders.settings.some-feature" />,
       component: SomeFeatureSettings,
     },
   ];
