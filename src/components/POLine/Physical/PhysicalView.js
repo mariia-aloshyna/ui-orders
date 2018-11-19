e726fc674489c89db2caccac77af427ea27dcbfa
Andrei_Shumski2 <Andrei_Shumski2@epam.com>
2018-11-19 16:46:49 +0300

    UIOR-45-Add-Physical-Resource-Details

--- src/components/POLine/Physical/PhysicalView.js
+++ src/components/POLine/Physical/PhysicalView.js
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
-import { KeyValue, Row, Col } from '@folio/stripes/components';
+import get from 'lodash/get';
+import toString from 'lodash/toString';
+import {
+  KeyValue,
+  Row,
+  Col
+} from '@folio/stripes/components';
+import FormatDate from '../../Utils/FormatDate';
 
-class PhysicalView extends React.Component {
+class PhysicalView extends Component {
   static propTypes = {
     initialValues: PropTypes.object
   }
 
   render() {
-    const { initialValues } = this.props;
+    const physical = this.props.initialValues.physical;
 
     return (
       <Row>
-        <Col xs={3}>
-          <KeyValue label="Expected Receipt Date" value={_.get(initialValues, 'expected_receipt_date')} />
+        <Col xs={6}>
+          <KeyValue
+            label={<FormattedMessage id="ui-orders.physical.materialSupplier" />}
+            value={get(physical, 'material_supplier')}
+          />
         </Col>
-        <Col xs={3}>
-          <KeyValue label="Receipt Note" value={_.get(initialValues, 'receipt_due')} />
+        <Col xs={6}>
+          <KeyValue
+            label={<FormattedMessage id="ui-orders.physical.receiptDue" />}
+            value={FormatDate(toString(get(physical, 'receipt_due')))}
+          />
         </Col>
-        <Col xs={3}>
-          <KeyValue label="Expected Receipt Interval" value={_.get(initialValues, 'expected_receipt_interval')} />
-        </Col>
-        <Col xs={3}>
-          <KeyValue label="Volumes" value={_.toString(_.get(initialValues, 'volumes'))} />
-        </Col>
-        <Col xs={3}>
-          <KeyValue label="Material ID" value={_.get(initialValues, 'material_id')} />
-        </Col>
-        <Col xs={3}>
-          <KeyValue label="Material Supplier" value={_.get(initialValues, 'material_supplier')} />
+        <Col xs={6}>
+          <KeyValue
+            label={<FormattedMessage id="ui-orders.physical.volumes" />}
+            value={toString(get(physical, 'volumes'))}
+          />
         </Col>
       </Row>
     );
--- src/components/POLine/const.js
+++ src/components/POLine/const.js
@@ -1,4 +1,6 @@
 export const ERESOURCE = 'Electronic Resource';
+export const PHYSICAL = 'Physical Resource';
 export const PE_MIX = 'P/E Mix';
 
 export const ERESOURCES = [ERESOURCE, PE_MIX];
+export const PHRESOURCES = [PHYSICAL, PE_MIX];
