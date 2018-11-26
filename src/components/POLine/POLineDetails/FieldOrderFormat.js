79fc801d39147f2a395fb69487893b7a65151083
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2018-11-26 12:50:22 +0300

    UIOR-52 PO Line Details: Update possible "Order Formats"

--- src/components/POLine/POLineDetails/FieldOrderFormat.js
+++ src/components/POLine/POLineDetails/FieldOrderFormat.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Field } from 'redux-form';
 
 import { Select } from '@folio/stripes/components';
 import { Required } from '../../Utils/Validate';
+import {
+  ERESOURCE,
+  PHYSICAL,
+  PE_MIX,
+  OTHER,
+} from '../const';
 
 const ORDER_FORMAT = {
-  container: 'Container',
-  electronicResource: 'Electronic Resource',
-  PEMix: 'P/E Mix',
-  physicalResource: 'Physical Resource',
-  service: 'Service',
+  electronicResource: ERESOURCE,
+  physicalResource: PHYSICAL,
+  PEMix: PE_MIX,
+  other: OTHER,
 };
 
-class FieldOrderFormat extends Component {
-  render() {
-    return (
-      <Field
-        component={Select}
-        label={<FormattedMessage id="ui-orders.poLine.orderFormat" />}
-        name="order_format"
-        validate={[Required]}
+const FieldOrderFormat = () => (
+  <Field
+    component={Select}
+    label={<FormattedMessage id="ui-orders.poLine.orderFormat" />}
+    name="order_format"
+    validate={[Required]}
+  >
+    <FormattedMessage id="ui-orders.dropdown.select">
+      {(message) => <option value="">{message}</option>}
+    </FormattedMessage>
+    {Object.keys(ORDER_FORMAT).map((key) => (
+      <FormattedMessage
+        id={`ui-orders.order_format.${key}`}
+        key={key}
       >
-        <FormattedMessage id="ui-orders.dropdown.select">
-          {(message) => <option value="">{message}</option>}
-        </FormattedMessage>
-        {Object.keys(ORDER_FORMAT).map((key) => (
-          <FormattedMessage
-            id={`ui-orders.order_format.${key}`}
-            key={key}
-          >
-            {(message) => <option value={ORDER_FORMAT[key]}>{message}</option>}
-          </FormattedMessage>
-        ))}
-      </Field>
-    );
-  }
-}
+        {(message) => <option value={ORDER_FORMAT[key]}>{message}</option>}
+      </FormattedMessage>
+    ))}
+  </Field>
+);
+
+FieldOrderFormat.displayName = 'FieldOrderFormat';
 
 export default FieldOrderFormat;
--- src/components/POLine/const.js
+++ src/components/POLine/const.js
@@ -1,6 +1,7 @@
 export const ERESOURCE = 'Electronic Resource';
 export const PHYSICAL = 'Physical Resource';
 export const PE_MIX = 'P/E Mix';
+export const OTHER = 'Other';
 
 export const ERESOURCES = [ERESOURCE, PE_MIX];
 export const PHRESOURCES = [PHYSICAL, PE_MIX];
