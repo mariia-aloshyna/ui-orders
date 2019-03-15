c62b00155490379108cf6f98c3b145b70cddd549
Andrei_Shumski2 <Andrei_Shumski2@epam.com>
2019-03-15 19:50:57 +0300

    revert piece factory

--- test/bigtest/network/factories/piece.js
+++ test/bigtest/network/factories/piece.js
@@ -5,8 +5,8 @@ import {
 
 export default Factory.extend({
   id: () => faker.random.uuid(),
-  poLineId: 'e9009acb-5e89-40b7-8b07-6d565f567778',
-  poLineNumber: '1000-1',
+  poLineId: () => faker.random.uuid(),
+  poLineNumber: (id) => `${id}${faker.random.alphaNumeric()}${faker.random.alphaNumeric()}${faker.random.alphaNumeric()}`,
   title: (id) => `Piece - ${id}`,
   dateOrdered: () => faker.date.past(),
   receivingNote: (poLineNumber) => `POLine Number: ${poLineNumber}`,
