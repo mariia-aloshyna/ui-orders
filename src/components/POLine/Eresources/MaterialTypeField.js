2eb7a7de147b225c93b2c50527746932b5924132
Andrei_Shumski2 <Andrei_Shumski2@epam.com>
2019-04-10 10:12:45 +0300

    UIOR-195 Certain locations prevent check-in

--- src/components/POLine/Eresources/MaterialTypeField.js
+++ src/components/POLine/Eresources/MaterialTypeField.js
@@ -5,13 +5,14 @@ import PropTypes from 'prop-types';
 
 import { Select } from '@folio/stripes/components';
 
+import { EMPTY_OPTION } from '../../Utils/const';
 import normalizeEmptySelect from '../../Utils/normalizeEmptySelect';
 import { requiredMaterialType } from '../../Utils/Validate';
 
 const MaterialTypeField = ({ name, materialTypes }) => (
   <Field
     component={Select}
-    dataOptions={[{ label: '', value: '' }, ...materialTypes]}
+    dataOptions={[EMPTY_OPTION, ...materialTypes]}
     fullWidth
     label={<FormattedMessage id="ui-orders.poLine.materialType" />}
     name={name}
