1a3d65b940d5c0e3c8ecf9db42556442e0554066
Aliaksei Chumakou <aliaksei_chumakou@epam.com>
2019-05-27 11:11:13 +0300

    UIOR-267 resources config

---
+++ src/OrderLinesList/constants.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
+export const FILTERS = {
+  RECEIPT_STATUS: 'receiptStatus',
+  DATE_CREATED: 'createdDate',
+  LOCATION: 'locations',
+};
---
+++ src/common/utils/index.js
@@ -0,0 +1,4 @@
+import getLocationOptions from './getLocationOptions';
+
+// eslint-disable-next-line import/prefer-default-export
+export { getLocationOptions };
