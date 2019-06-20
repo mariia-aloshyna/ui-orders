26d77a2a41ff91b043d711ed5194d9a64a030598
Mikita Siadykh <mikita_siadykh@epam.com>
2019-06-20 14:33:46 +0300

    UIOR-237 accordions set up for order templates

--- src/settings/OrderTemplates/OrderTemplates.js
+++ src/settings/OrderTemplates/OrderTemplates.js
@@ -1,39 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Switch from 'react-router-dom/Switch';
 import Route from 'react-router-dom/Route';
 import PropTypes from 'prop-types';
 import ReactRouterPropTypes from 'react-router-prop-types';
 
+import {
+  SUFFIXES_SETTING,
+  PREFIXES_SETTING,
+} from '../../components/Utils/resources';
 import OrderTemplatesList from './OrderTemplatesList';
 import OrderTemplatesEditor from './OrderTemplatesEditor';
 
-const OrderTemplates = ({ match, label }) => {
-  const { path } = match;
+class OrderTemplates extends Component {
+  static manifest = Object.freeze({
+    prefixesSetting: PREFIXES_SETTING,
+    suffixesSetting: SUFFIXES_SETTING,
+  });
 
-  return (
-    <Switch>
-      <Route
-        exact
-        path={path}
-        render={() => (
-          <OrderTemplatesList
-            label={label}
-            rootPath={path}
-          />
-        )}
-      />
-      <Route
-        exact
-        path={`${path}/create`}
-        render={() => <OrderTemplatesEditor />}
-      />
-    </Switch>
-  );
-};
+  static propTypes = {
+    label: PropTypes.object.isRequired,
+    match: ReactRouterPropTypes.match.isRequired,
+    history: ReactRouterPropTypes.history.isRequired,
+  };
 
-OrderTemplates.propTypes = {
-  label: PropTypes.object.isRequired,
-  match: ReactRouterPropTypes.match.isRequired,
-};
+  closePane = () => {
+    const { history, match: { path } } = this.props;
+
+    history.push(path);
+  }
+
+  render() {
+    const { label, match: { path } } = this.props;
+
+    return (
+      <Switch>
+        <Route
+          exact
+          path={path}
+          render={() => (
+            <OrderTemplatesList
+              label={label}
+              rootPath={path}
+            />
+          )}
+        />
+        <Route
+          exact
+          path={`${path}/create`}
+          render={() => (
+            <OrderTemplatesEditor
+              close={this.closePane}
+            />
+          )}
+        />
+      </Switch>
+    );
+  }
+}
 
 export default OrderTemplates;
---
+++ src/settings/OrderTemplates/constants.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import { ACCORDION_ID as PO_LINE_ACCORDION } from '../../components/POLine/const';
+
+export const ORDER_TEMPLATES_ACCORDION = {
+  TEMPLATE_INFO: 'templateInfo',
+  PO_INFO: 'poInfo',
+  PO_NOTES: 'poNotes',
+  PO_SUMMARY: 'poSummary',
+  POL_ITEM_DETAILS: PO_LINE_ACCORDION.itemDetails,
+  POL_DETAILS: PO_LINE_ACCORDION.lineDetails,
+  POL_COST_DETAILS: PO_LINE_ACCORDION.costDetails,
+  POL_VENDOR: PO_LINE_ACCORDION.vendor,
+  POL_FUND_DISTIBUTION: PO_LINE_ACCORDION.fundDistribution,
+  POL_ERESOURCES: PO_LINE_ACCORDION.eresources,
+  POL_FRESOURCES: PO_LINE_ACCORDION.physical,
+  POL_LOCATION: PO_LINE_ACCORDION.location,
+};
+
+export const ORDER_TEMPLATES_ACCORDION_TITLES = {
+  [ORDER_TEMPLATES_ACCORDION.TEMPLATE_INFO]: <FormattedMessage id="ui-orders.settings.orderTemplates.accordion.template" />,
+  [ORDER_TEMPLATES_ACCORDION.PO_INFO]: <FormattedMessage id="ui-orders.settings.orderTemplates.accordion.poInfo" />,
+  [ORDER_TEMPLATES_ACCORDION.PO_NOTES]: <FormattedMessage id="ui-orders.settings.orderTemplates.accordion.poNotes" />,
+  [ORDER_TEMPLATES_ACCORDION.PO_SUMMARY]: <FormattedMessage id="ui-orders.settings.orderTemplates.accordion.poSummary" />,
+  [ORDER_TEMPLATES_ACCORDION.POL_ITEM_DETAILS]: <FormattedMessage id="ui-orders.settings.orderTemplates.accordion.poItemDetails" />,
+  [ORDER_TEMPLATES_ACCORDION.POL_DETAILS]: <FormattedMessage id="ui-orders.settings.orderTemplates.accordion.polDetails" />,
+  [ORDER_TEMPLATES_ACCORDION.POL_COST_DETAILS]: <FormattedMessage id="ui-orders.settings.orderTemplates.accordion.polCostDetails" />,
+  [ORDER_TEMPLATES_ACCORDION.POL_VENDOR]: <FormattedMessage id="ui-orders.settings.orderTemplates.accordion.polVendor" />,
+  [ORDER_TEMPLATES_ACCORDION.POL_FUND_DISTIBUTION]: <FormattedMessage id="ui-orders.settings.orderTemplates.accordion.polFundDistribution" />,
+  [ORDER_TEMPLATES_ACCORDION.POL_ERESOURCES]: <FormattedMessage id="ui-orders.settings.orderTemplates.accordion.polEResources" />,
+  [ORDER_TEMPLATES_ACCORDION.POL_FRESOURCES]: <FormattedMessage id="ui-orders.settings.orderTemplates.accordion.polFResources" />,
+  [ORDER_TEMPLATES_ACCORDION.POL_LOCATION]: <FormattedMessage id="ui-orders.settings.orderTemplates.accordion.polLocation" />,
+};
